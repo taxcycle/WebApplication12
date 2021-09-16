d61d32bbbf8b80f4b3b717af399149e3d71a06b9
Colan Toews <colan@taxcycle.com>
2021-09-16 10:03:01 -0600

    Add project files.

---
+++ WebApplication12/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Xero.NetStandard.OAuth2.Client;
+using Xero.NetStandard.OAuth2.Config;
+
+namespace WebApplication12.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var xeroConfig = new XeroConfiguration();
+            xeroConfig.ClientId = "your client id";
+            xeroConfig.ClientSecret = "your client secret";
+            xeroConfig.CallbackUri = new Uri("https://b53a-206-174-203-243.ngrok.io/try-now?ssu=xero");
+            xeroConfig.Scope = "openid profile email accounting.settings.read";
+            var xeroClient = new XeroClient(xeroConfig);
+            var loginUri = xeroClient.BuildLoginUri();
+            Debug.WriteLine(loginUri);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
